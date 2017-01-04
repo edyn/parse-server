d3a4479ac01cdc34c6fcabd0e5e4eddeab903a95
Bobo Diallo <bobo.diaraye@gmail.com>
2017-01-04 18:13:51 +0000

    Add health endpoint

--- index.js
+++ index.js
@@ -72,6 +72,10 @@ app.get('/', function(req, res) {
   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
+app.get('/_health', function(req, res) {
+  res.status(200).send('Ok');
+});
+
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
 app.get('/test', function(req, res) {
